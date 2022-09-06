8e4885c9c55bdcf17377217e62bf396c8279ba89
Moises Abraham Vazquez Perez <mavazquezp@bancoazteca.com>
2022-09-06 15:09:16 -0500

    [ADD] Addition de Marks

--- GitExampleApp/GitExampleApp/ViewController.swift
+++ GitExampleApp/GitExampleApp/ViewController.swift
@@ -4,11 +4,15 @@
 //
 //  Created by Daniel BR on 03/09/22.
 //
-
+//MARK: - L I B R A R I E S
 import UIKit
 
+
+//MARK: - C L A S S
 class ViewController: UIViewController {
 
+    
+    //MARK: - L I F E · C Y C L E
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
